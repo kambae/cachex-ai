6cd8cdddba2784aeb444e4e896951d3661cf814d
Ryan <ryanwp526@gmail.com>
2022-05-03 05:00:10 +1000

    Negamax implemented - not working
    Much naively

--- negamax/player.py
+++ negamax/player.py
@@ -33,10 +33,15 @@ class Player:
 
         return ("PLACE", *action)
 
-
+    # player_num = 1 for player and player_num = -1 for opponent
     def negamax(self, board, depth, alpha, beta, player_num):
         # todo: detect game end by repeat state/turns as well
-        if depth == 0: # todo: or node is a terminal node
+        # check if node is terminal
+        winner = self.check_winner(board)
+        if winner:
+            return player_num * (math.inf if self.player == winner else -math.inf)
+
+        if depth == 0:
             return player_num * self.evaluate(board)
 
         next_states = self.get_legal_moves(board)[1]
@@ -52,16 +57,70 @@ class Player:
         return value
 
     def get_legal_moves(self, board):
-        board_values = [(i // board, i % board) for i in range(0, np.square(board))]
-        action_set = [i for i in board_values if board.is_occupied(i)]
+        board_values = [(i // board.n, i % board.n) for i in range(0, np.square(board.n))]
+        action_set = [i for i in board_values if not board.is_occupied(i)]
         next_states = [copy.deepcopy(board) for i in action_set]
         map(lambda i: next_states[i].place(self.player, action_set[i]), range(0, len(action_set)))
 
         return action_set, next_states
 
-    def evaluate(self, board):
-        return 0
+    # todo: this bit is kinda shit, refactor if necessary
+    def check_winner(self, board):
+        if self.get_player_min_placements(board, "red") == 0:
+            return "red"
+        elif self.get_player_min_placements(board, "blue") == 0:
+            return "blue"
+        return None
 
+    # todo: this bit is kinda shit, refactor if necessary
+    def evaluate(self, board):
+        enemy = "blue" if self.player == "red" else "red"
+        return self.get_player_min_placements(board, enemy) - self.get_player_min_placements(board, self.player)
+
+    # apply dijkstra
+    # todo: this bit is kinda shit, refactor if necessary, may need to memoise
+    def get_player_min_placements(self, board, player):
+        def path_heuristic(a):
+            return 0
+
+        def get_neighbours(a):
+            if a == (-1, -1):
+                return [(0, i) for i in range(0, n) if is_valid_neighbour((0, i))] if player == "red" else [(i, 0) for i in range(0, n) if is_valid_neighbour((i, 0))]
+
+            neighbours = [i for i in board._coord_neighbours(a) if is_valid_neighbour(i)]
+
+            if (player == "red" and a[0] == n - 1) or (player == "blue" and a[1] == n - 1):
+                neighbours.append((n, n))
+
+            return neighbours
+
+        def get_edge_weight(a, b):
+            if a == (-1, -1) or b == (n, n) or board[b] == player:
+                return 0
+            return 1
+
+        def is_valid_neighbour(a):
+            return board[a] == player or board[a] is None
+
+        n = board.n
+        start = (-1, -1)
+        goal = (n, n)
+
+        prev = {start: None}
+        dist = {start: 0}
+        pq = PriorityQueue()
+        pq.insert(start, path_heuristic(start))
+        while not pq.is_empty():
+            curr = pq.pop()
+            if curr == goal:
+                return dist[goal]
+            for neighbour in get_neighbours(curr):
+                tentative_dist = dist[curr] + get_edge_weight(curr, neighbour)
+                if neighbour not in dist or tentative_dist < dist[neighbour]:
+                    pq.update(neighbour, tentative_dist + path_heuristic(neighbour))
+                    prev[neighbour] = curr
+                    dist[neighbour] = tentative_dist
+        return None
 
     
     def turn(self, player, action):
@@ -75,3 +134,28 @@ class Player:
             # Apply PLACE action
             coord = tuple(aargs)
             self.board.place(player, coord)
+
+# todo make prio queue efficient?
+class PriorityQueue:
+    def __init__(self):
+        self.items = []
+
+    def insert(self, item, prio):
+        self.items.append({"prio": prio, "item": item})
+
+    def pop(self):
+        if len(self.items) == 0:
+            return None
+        self.items = sorted(self.items, key=lambda x: x["prio"])
+        return self.items.pop(0)["item"]
+
+    # removes all matches to item and replaces with new prio
+    def update(self, item, prio):
+        self.items = list(filter(lambda x: x["item"] != item, self.items))
+        self.insert(item, prio)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def __str__(self):
+        return str(self.items)
