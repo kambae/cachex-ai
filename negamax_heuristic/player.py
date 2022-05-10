d507453f9a23c0bab60e66238aede54f18ae7c7c
Saurabh <jhanjeesaurabh@gmail.com>
2022-05-11 00:32:58 +1000

    improvements-actually this time. better heuristic.

--- negamax_heuristic/player.py
+++ negamax_heuristic/player.py
@@ -3,6 +3,7 @@ from negamax import player
 
 class Player(player.Player):
     depth = 2
+
     def evaluate(self, board):
         player_hexes = board.red_hexes if self.player == "red" else board.blue_hexes
         enemy_hexes = board.blue_hexes if self.enemy == "blue" else board.red_hexes
--- negamax_heuristic_2/player.py
+++ negamax_heuristic_2/player.py
@@ -1,4 +1,6 @@
 from negamax import player
+import numpy as np
+from functools import lru_cache
 
 
 class Player(player.Player):
@@ -8,18 +10,11 @@ class Player(player.Player):
         player_hexes = board.red_hexes if self.player == "red" else board.blue_hexes
         enemy_hexes = board.blue_hexes if self.enemy == "blue" else board.red_hexes
 
-        def get_total_tile_value(board_input, player_type, hexes):
-            triples = set()
-            for hex in hexes:
-                candidates = set()
-                for coord in board_input._coord_neighbours(hex):
-                    if board_input[coord] == player_type:
-                        candidates.add(coord)
-                for i in candidates:
-                    for coord in board_input._coord_neighbours(i):
-                        if coord in candidates:
-                            triples.add(frozenset({hex, i, coord}))
-            return len(triples)
+        def get_centrality_score(hexes):
+            return - sum([get_hex_centrality(hex) for hex in hexes])
 
-        return 10/self.get_player_min_placements(board, self.player) - 20/self.get_player_min_placements(board, self.enemy) + len(player_hexes) - len(enemy_hexes)
+        @lru_cache(maxsize=None)
+        def get_hex_centrality(hex):
+            return np.linalg.norm(np.array([hex]) - np.array([self.n / 2, self.n / 2]))
 
+        return self.get_player_min_placements(board, self.enemy) - self.get_player_min_placements(board, self.player)  + 1/1000 * (len(player_hexes) - len(enemy_hexes)) + 1/100000 * get_centrality_score(player_hexes)
